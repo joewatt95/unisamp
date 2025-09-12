18c01dc34c32c3b4e8a4a98b1abdde896dedc130
Joe Watt <joewatt95@u.nus.edu>
2025-09-12 12:44:50 +0800

    get sbva to use vcpkg deps

--- vcpkg_overlay_ports/sbva/portfile.cmake
+++ vcpkg_overlay_ports/sbva/portfile.cmake
@@ -3,6 +3,8 @@ vcpkg_from_github(
   REPO meelgroup/sbva
   REF 0faa08cf3cc26ed855831c9dc16a3489c9ae010f
   SHA512 c9f6fb54b5e6d201f5acf71472d4eeee4952a39f14c3289fa94e920077b98f8699c83090821789278bc749b56e2c5945397de615bbce1c8db707fac5b87ed1cb
+  PATCHES
+    use_vcpkg_deps.patch
 )
 
 vcpkg_cmake_configure(
