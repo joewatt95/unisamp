2aa077000b2c9f52b5418d3ffe7e1bc6581f2ac5
Joe Watt <joewatt95@u.nus.edu>
2025-10-31 09:29:19 +0800

    split to diff repo

--- experiments/time_and_plot.sh
+++ experiments/time_and_plot.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd "$(dirname "$0")/time_and_plot" || exit
-
-taskset -c 0,2,4,6,8,10,12,14 poetry run python -m time_and_plot \
-  "../../build/unigen_static -e 0.3 --samples {} ../benchmarks/dummy.cnf" \
-  -p 8 -r 10 --output-plot ../timing_plot.svg --output-stats ../timing_plot_stats.csv \
+taskset -c 0,2,4,6,8,10,12,14 \
+  poetry --directory "$(dirname "$0")" --project ~/dev/time_and_plot \
+  run python -m time_and_plot \
+  "../build/unigen_static -e 0.3 --samples {} benchmarks/dummy.cnf" \
+  -p 8 -r 10 --output-plot timing_plot.svg --output-stats timing_plot_stats.csv \
   $(seq 10 10 5000)
   # 1.1 1.2 1.3 1.4 1.5 1.52 1.6
   #  "../../build/unigen_static -e 0.3 -r {} --samples 1 ../benchmarks/LoginService.sk_20_34.cnf.gz.no_w.cnf" \
