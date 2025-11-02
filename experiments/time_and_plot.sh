2edb54fd0e2a235dae940ab0c9f82b8e1bcc9b34
joewatt95 <joewatt95@u.nus.edu>
2025-11-02 18:13:42 +0800

    wip

--- experiments/time_and_plot.sh
+++ experiments/time_and_plot.sh
@@ -3,7 +3,6 @@
 taskset -c 0,2,4,6,8,10,12,14 \
   poetry --directory "$(dirname "$0")" --project ~/dev/time_and_plot \
     run python -m time_and_plot \
-      "../build/unigen_static -e 0.3 --samples 100 benchmarks/{}.cnf" \
+      "../build/unigen_static -e 0.3 -r 1.6 --samples {} benchmarks/GuidanceService.sk_4_27.cnf.gz.no_w.cnf" \
       -p 8 -r 20 --output-plot timing_plot.svg --output-stats timing_plot_stats.csv \
-      "GuidanceService.sk_4_27.cnf.gz.no_w" "LoginService.sk_20_34.cnf.gz.no_w"
-      # $(seq 10 10 5000)
+      $(seq 50 50 1000)
